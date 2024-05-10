6bf3fd14f003b4c543f0ae23653b8e6e194239c3
Shubham <kuanarshubham@gmail.com>
2024-05-10 15:35:11 +0530

    Static Portfolio

---
+++ index.js
@@ -0,0 +1,8 @@
+const linksKids = document.getElementsByClassName("links-kids");
+const otherP = document.getElementsByClassName("other-p");
+console.log(otherP);
+const getInTouch = document.getElementsByClassName("get-in-touch");
+
+otherP[0].addEventListener('click', () => {window.location.href = 'https://google.com';});
+
+
